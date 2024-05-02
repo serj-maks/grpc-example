cf20cd46b68b38d857e3bbdc8feb5e888d4714f2
Sergey Maksimov <sergeymaksimov1993@gmail.com>
2024-05-02 16:51:26 +0300

    add client streaming call (client sends stream of messages, server responds with a single message)

--- client/src/main/java/org/example/Client.java
+++ client/src/main/java/org/example/Client.java
@@ -2,6 +2,7 @@ package org.example;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -10,11 +11,14 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.example.serverproto.ServerGrpc;
-import org.example.serverproto.ServerPropertyName;
-import org.example.serverproto.ServerPropertyValue;
+import org.example.serverproto.*;
 
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 @Path("/properties")
@@ -36,12 +40,69 @@ public class Client {
         ManagedChannel channel = ManagedChannelBuilder.forAddress(SYSTEM_HOST, SYSTEM_PORT)
                 .usePlaintext()
                 .build();
-        ServerGrpc.ServerBlockingStub client = ServerGrpc.newBlockingStub(channel);
+        ServerGrpc.ServerBlockingStub server = ServerGrpc.newBlockingStub(channel);
         ServerPropertyName request = ServerPropertyName.newBuilder()
                 .setPropertyName(property)
                 .build();
-        ServerPropertyValue response = client.getProperty(request);
+        ServerPropertyValue response = server.getProperty(request);
         channel.shutdown();
         return response.getPropertyValue();
+    }
+
+    @GET
+    @Path("/user")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Properties getUserProperties() {
+
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress(SYSTEM_HOST, SYSTEM_PORT)
+                .usePlaintext().build();
+        ServerGrpc.ServerStub client = ServerGrpc.newStub(channel);
+        CountDownLatch countDown = new CountDownLatch(1);
+        Properties properties = new Properties();
+
+        StreamObserver<SystemPropertyName> stream = client.getClientStreamingProperties(
+                new StreamObserver<SystemProperties>() {
+
+                    @Override
+                    public void onNext(SystemProperties value) {
+                        logger.info("client streaming received a map that has "
+                                + value.getPropertiesCount() + " properties");
+                        properties.putAll(value.getPropertiesMap());
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        t.printStackTrace();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        logger.info("client streaming completed");
+                        countDown.countDown();
+                    }
+                });
+
+        // collect the property names starting with user.
+        List<String> keys = System.getProperties().stringPropertyNames().stream()
+                .filter(k -> k.startsWith("user."))
+                .collect(Collectors.toList());
+
+        // send messages to the server
+        keys.stream()
+                .map(k -> SystemPropertyName.newBuilder().setPropertyName(k).build())
+                .forEach(stream::onNext);
+        stream.onCompleted();
+
+        // wait until completed
+        try {
+            countDown.await(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        channel.shutdownNow();
+
+        return properties;
     }
 }
--- server/src/main/java/org/example/Server.java
+++ server/src/main/java/org/example/Server.java
@@ -1,10 +1,10 @@
 package org.example;
 
 import io.grpc.stub.StreamObserver;
-import org.example.serverproto.ServerGrpc;
-import org.example.serverproto.ServerPropertyName;
-import org.example.serverproto.ServerPropertyValue;
+import org.example.serverproto.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 public class Server extends ServerGrpc.ServerImplBase {
@@ -23,5 +23,38 @@ public class Server extends ServerGrpc.ServerImplBase {
                 .build();
         streamObserver.onNext(value);
         streamObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<SystemPropertyName> getClientStreamingProperties(
+            StreamObserver<SystemProperties> observer) {
+
+        return new StreamObserver<SystemPropertyName>() {
+
+            private Map<String, String> properties = new HashMap<String, String>();
+
+            @Override
+            public void onNext(SystemPropertyName spn) {
+                String pName = spn.getPropertyName();
+                String pValue = System.getProperty(pName);
+                logger.info("client streaming received property: " + pName);
+                properties.put(pName, pValue);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                t.printStackTrace();
+            }
+
+            @Override
+            public void onCompleted() {
+                SystemProperties value = SystemProperties.newBuilder()
+                        .putAllProperties(properties)
+                        .build();
+                observer.onNext(value);
+                observer.onCompleted();
+                logger.info("client streaming was completed!");
+            }
+        };
     }
 }
